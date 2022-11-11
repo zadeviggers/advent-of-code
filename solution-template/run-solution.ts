29a809ecfff43678fb9c41e082efededaf72188b
Zade Viggers <zade@viggers.net>
2022-11-12 10:40:01 +1300

    Clen

--- solution-template/run-solution.ts
+++
@@ -1,7 +0,0 @@
-import { solution } from "./solution.ts";
-
-const input = await Deno.readTextFile("./sample-input.txt");
-
-const output = await solution(input);
-
-await Deno.writeTextFile("output.txt", output);
--- solution-template/solution.ts
+++
@@ -1,3 +0,0 @@
-export async function solution(input: string): Promise<string> {
-  return input;
-}
