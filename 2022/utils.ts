e38586a36c510f9e2c3d887a3f7a384751e52513
Zade Viggers <zade@viggers.net>
2022-12-02 15:57:57 +1300

    Do cool things

--- 2022/utils.ts
+++ 2022/utils.ts
@@ -1,21 +1,44 @@
-export type AnyNestedNumberArray =
-  | number[]
-  | number[][]
-  | number[][][]
-  | number[][][][]
-  | number[][][][][]
-  | number[][][][][][];
+export type AnyDepthNestedArray<T> = Array<T | AnyDepthNestedArray<T>>;
 
-export function sum(...toSum: AnyNestedNumberArray): number {
+/**
+ *
+ * @example
+ * ```ts
+ * flatten(1, 2, [3, 4, [[[5]]], 6, 7, 8], 9, [10, 11, [12, [13], 14]])
+ * // Returns [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+ * ```
+ */
+export function flatten<T>(...toFlatten: AnyDepthNestedArray<T>): T[] {
+  // Cast Infinity as integer to get around a typescript bug: https://github.com/microsoft/TypeScript/issues/49280
+  return toFlatten.flat(Infinity as 1) as T[];
+}
+
+/**
+ *
+ * @example
+ * ```ts
+ * sum(1, 2, [3, 4, [[[5]]], 6, 7, 8], 9, [10, 11, [12, [13], 14]])
+ * // Returns 105
+ * ```
+ */
+export function sum(...toSum: AnyDepthNestedArray<number>): number {
   let sum = 0;
-  for (const num of toSum.flat(Infinity) as number[]) {
+  for (const num of flatten<number>(...toSum)) {
     sum += num;
   }
   return sum;
 }
 
-export function average(...toAverage: AnyNestedNumberArray): number {
-  const total = sum(toAverage.flat(Infinity) as number[]);
+/**
+ *
+ * @example
+ * ```ts
+ * average(1, 2, [3, 4, [[[5]]], 6, 7, 8], 9, [10, 11, [12, [13], 14]])
+ * // Returns 21
+ * ```
+ */
+export function average(...toAverage: AnyDepthNestedArray<number>): number {
+  const total = sum(...toAverage);
   const avg = total / toAverage.length;
   return avg;
 }
