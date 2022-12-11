09348657535026ef3d54d18296e0bd8d5fa709e1
Zade Viggers <zade@viggers.net>
2022-12-12 12:43:11 +1300

    Improve tests

--- 2022/utils.test.ts
+++ 2022/utils.test.ts
@@ -11,54 +11,92 @@ import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 
 Deno.test({
   name: "flatten() example",
-  fn() {
-    const res = flatten(1, 2, [3, 4, [[[5]]], 6, 7, 8], 9, [
-      10,
-      11,
-      [12, [13], 14],
-    ]);
-    assertEquals(res, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
+
+  fn: async (t) => {
+    await t.step("Simple", () => {
+      const res = flatten([
+        [567, 890],
+        [0, -1],
+        [8, 6],
+      ]);
+      assertEquals(res, [567, 890, 0, -1, 8, 6]);
+    });
+
+    await t.step("Complex array", () => {
+      const res = flatten(1, 2, [3, 4, [[[5]]], 6, 7, 8], 9, [
+        10,
+        11,
+        [12, [13], 14],
+      ]);
+      assertEquals(res, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
+    });
+
+    await t.step("Already flattened", () => {
+      const res = flatten([[567, 890, 0, -1, 8, 6]]);
+      assertEquals(res, [567, 890, 0, -1, 8, 6]);
+    });
   },
 });
 
 Deno.test({
-  name: "sum() example",
-  fn() {
-    const res = sum(1, 2, [3, 4, [[[5]]], 6, 7, 8], 9, [
-      10,
-      11,
-      [12, [13], 14],
-    ]);
-    assertEquals(res, 105);
+  name: "sum()",
+  fn: async (t) => {
+    await t.step("Simple", () => {
+      const res = sum([567, 890, 0, -1, 8]);
+      assertEquals(res, 1464);
+    });
+
+    await t.step("Complex array", () => {
+      const res = sum(1, 2, [3, 4, [[[5]]], 6, 7, 8], 9, [
+        10,
+        11,
+        [12, [13], 14],
+      ]);
+      assertEquals(res, 105);
+    });
   },
 });
 
 Deno.test({
-  name: "average() example",
-  fn() {
-    const res = average(1, 2, [3, 4, [[[5]]], 6, 7, 8], 9, [
-      10,
-      11,
-      [12, [13], 14],
-    ]);
-    assertEquals(res, 7.5);
+  name: "average()",
+  fn: async (t) => {
+    await t.step("Simple", () => {
+      const res = average([567, 890, 0, -1, 8]);
+      assertEquals(res, 292.8);
+    });
+
+    await t.step("Complex array", () => {
+      const res = average(1, 2, [3, 4, [[[5]]], 6, 7, 8], 9, [
+        10,
+        11,
+        [12, [13], 14],
+      ]);
+      assertEquals(res, 7.5);
+    });
   },
 });
 
 Deno.test({
-  name: "sort() example",
-  fn() {
-    const res = sort(1, 2, [3, 4, [[[5]]], 6, 7, 8], 9, [
-      10,
-      11,
-      [12, [13], 14],
-    ]);
-    assertEquals(res, [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+  name: "sort()",
+  fn: async (t) => {
+    await t.step("Simple", () => {
+      const res = sort([567, 890, 0, -1, 8]);
+      assertEquals(res, [890, 567, 8, 0, -1]);
+    });
+
+    await t.step("Complex array", () => {
+      const res = sort(1, 2, [3, 4, [[[5]]], 6, 7, 8], 9, [
+        10,
+        11,
+        [12, [13], 14],
+      ]);
+      assertEquals(res, [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+    });
   },
 });
 
 Deno.test({
-  name: "splitLines() example",
+  name: "splitLines()",
   fn() {
     const res = splitLines(`a
 
@@ -72,44 +110,49 @@ jfdk
 });
 
 Deno.test({
-  name: "XOR() example",
-  fn() {
-    assertEquals(XOR(true, true), false);
+  name: "XOR()",
+  fn: async (t) => {
+    await t.step("Both true", () => assertEquals(XOR(true, true), false));
 
-    assertEquals(XOR(true, false), true);
+    await t.step("Both false", () => assertEquals(XOR(false, false), false));
 
-    assertEquals(XOR(false, false), false);
+    await t.step("One true, one false", () =>
+      assertEquals(XOR(true, false), true)
+    );
   },
 });
 
 Deno.test({
-  name: "range() with .next()",
-  fn() {
-    const res = range(3);
-    assertEquals(res.next().value, 0);
-    assertEquals(res.next().value, 1);
-    assertEquals(res.next().value, 2);
+  name: "range()",
+  fn: async (t) => {
+    await t.step("With .next()", async (t) => {
+      const res = range(3);
 
-    const final = res.next();
-    assertEquals(final.value, undefined);
-    assertEquals(final.done, true);
-  },
-});
+      await t.step("Correct values returned", () => {
+        assertEquals(res.next().value, 0);
+        assertEquals(res.next().value, 1);
+        assertEquals(res.next().value, 2);
+      });
 
-Deno.test({
-  name: "range() in for...of loop - simple",
-  fn() {
-    const res = [];
-    for (const i of range(2)) res.push(i);
-    assertEquals(res, [0, 1]);
-  },
-});
+      await t.step("Finishes correctly", () => {
+        const final = res.next();
+        assertEquals(final.value, undefined);
+        assertEquals(final.done, true);
+      });
+    });
 
-Deno.test({
-  name: "range() in for...of loop - complex",
-  fn() {
-    const res = [];
-    for (const i of range(12, 2)) res.push(i);
-    assertEquals(res, [0, 2, 4, 6, 8, 10]);
+    await t.step("In a for...of loop", async (t) => {
+      await t.step("Simple", () => {
+        const res = [];
+        for (const i of range(2)) res.push(i);
+        assertEquals(res, [0, 1]);
+      });
+
+      await t.step("Complex", () => {
+        const res = [];
+        for (const i of range(12, 2)) res.push(i);
+        assertEquals(res, [0, 2, 4, 6, 8, 10]);
+      });
+    });
   },
 });
