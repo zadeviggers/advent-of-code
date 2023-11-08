768597e7900df9abf36a7baa136d7f5f1a80cb70
Zade Viggers <zade@viggers.net>
2023-11-08 20:21:33 +1300

    Typo

--- 2020/day2/part2.rb
+++ 2020/day2/part2.rb
@@ -5,7 +5,7 @@ regex = /([0-9]+)-([0-9]+) ([a-z]): (.+)/
 input.each do |line|
     a, b, letter, password = line.match(regex).captures
     count = password.count letter 
-    # Using the bitwise OR operator (^) seems dodgy, but it should work
+    # Using the bitwise XOR operator (^) seems dodgy, but it should work
     if (password[a.to_i() - 1] == letter) ^ (password[b.to_i() - 1] == letter)
         valid_amount += 1
     end
